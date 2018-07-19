c6d63148d9607ae753050a48f4943abb1a8c43cf
Aziz <azizulin2@gmail.com>
2018-07-19 14:09:35 +0500

    Remaking of the SeeFood app from the Silicon Valley. This version is using IBM Bluemix and API calls instead of the CoreML functionality.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let API_KEY: String = "vMAR1awvUidlgWlK5flJeXonmcIZIwrbGobpaYY-BaHa"
+    let version: String = "2018-07-19"
 
+    @IBOutlet var cameraButton: UIBarButtonItem!
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: API_KEY)
+            
+            let imageData = UIImageJPEGRepresentation(image, 0.01)
+            
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            
+            let fileURL = documentsURL.appendingPathComponent("tempImage.jpg")
+            
+            try? imageData?.write(to: fileURL)
+            
+            visualRecognition.classify(imagesFile: fileURL) { (classifiedImages) in
+                print(classifiedImages)
+            }
+        }
+        else {
+            print("the image is nil")
+        }
+        
     }
-
-
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    
 }
 
